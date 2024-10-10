dc7ca2243020d0bec380a7f31224946c5ee10f15
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-10-10 05:37:23 -0600

    Add files via upload

---
+++ pr2_2do_parcial_Lira_Hernandez_Dayana_1191_3W_2.py
@@ -0,0 +1,11 @@
+print(" ")#espacio a agregar a la ejecucion 
+print("Lira Hernandez Dayana Yamileth, 1191, 3W")#datos del realizador
+print("Manda saludo a el nombre ingresado")#
+print(" ")#espacio a agregar a la ejecucion
+
+# Solicitar al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre:")
+
+# Desplegar el saludo personalizado
+print(f"¡Hola {nombre}!")#imprime el saludo a ejecutar
+print(" ")#imprime espacio a agregar a la ejecucion
